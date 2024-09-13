9a1f51fd09059575a1f4d0d0c5bcf6e216e13436
Nilamroychoudhury1999 <63612266+nilamroychoudhury1999@users.noreply.github.com>
2024-09-13 13:46:44 +0530

    name

--- src/config-global.ts
+++ src/config-global.ts
@@ -10,6 +10,6 @@ export type ConfigValue = {
 // ----------------------------------------------------------------------
 
 export const CONFIG: ConfigValue = {
-  appName: 'Minimal UI',
+  appName: 'Quick Bill',
   appVersion: packageJson.version,
 };
